695d786a76afc4f848d838ea446b64dafbae2f2a
wesleyhuang23 <wesleyhuang23@gmail.com>
2016-10-18 21:32:58 -0600

    reverser

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ $(document).ready(function(){
   $('.box').on('dblclick', function(){
     $(this).removeClass('white');
   });
+
+  $('#reset').on('click', function() {
+  $('.box').removeClass('white');
+});
 });
